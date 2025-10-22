d95d08247d6939988b6c7abcc65b62843c122c87
VadimVibe <kolesnikov.vadim2007@gmail.com>
2025-10-22 21:48:32 +0300

    Функция print_play_game()
    Показывает номера ячеек на игровом поле.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def print_place():
-    print(place[1], '|', place[2], '|', place[3])
-    print('--+---+--')
-    print(place[4], '|', place[5], '|', place[6])
-    print('--+---+--')
-    print(place[7], '|', place[8], '|', place[9])
+def print_play_game():
+    print("Игровое поле с номерами ячеек")
+    print(' 1 | 2 | 3 ')
+    print('---+---+---')
+    print(' 4 | 5 | 6 ')
+    print('---+---+---')
+    print(' 7 | 8 | 9 ')
