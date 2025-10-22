95eab2fe898250fe6c808c853fcb011c5c6a02ec
VadimVibe <kolesnikov.vadim2007@gmail.com>
2025-10-22 21:47:30 +0300

    Функция print_place()
    Выводит на экран позиции крестиков и ноликов на игровом поле.

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-'pustota'
+def print_place():
+    print(place[1], '|', place[2], '|', place[3])
+    print('--+---+--')
+    print(place[4], '|', place[5], '|', place[6])
+    print('--+---+--')
+    print(place[7], '|', place[8], '|', place[9])
