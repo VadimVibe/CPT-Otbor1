8c5ac1b6f98130fba1bcbfc81bc82375fc96fa6f
VadimVibe <kolesnikov.vadim2007@gmail.com>
2025-10-22 21:51:19 +0300

    Данные
    Просто данные

--- main.py
+++ main.py
@@ -1,7 +1,2 @@
-def if_win():
-    for i in range(0,7,3):
-        if place[1+i] == place[2+i] == place[3+i] and place[1+i] != ' ': return 1
-    for i in range(0,3):
-        if place[1+i] == place[4+i] == place[7+i] and place[1+i] != ' ': return 1
-    if (place[1] == place[5] == place[9] and place[1] != ' ') or (place[3] == place[5] == place[7] and place[3] != ' '): return 1
-    return  0
+place,hod_pl,hod, hod_zn = [' ']*10,['первый','второй','третий','четвертый','пятый'], 0, ['О','X']
+name = ['Игрок 1','Игрок 2']
